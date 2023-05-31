6fb4f1e2279fe23a5c39d872fd353fcdfe26ae07
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-06-01 01:01:44 +0300

    initialized response

---
+++ SteamMarketplace/Entities/Response/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace SteamMarketplace.Entities.Response
+{
+    public class Response
+    {
+        public string Message { get; set; }
+
+        public int HttpStatus { get; set; }
+
+        public object Data { get; set; }
+    }
+}
