7d399d39a9cde16f5a82add6e03129586e0e93dd
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-05-31 22:24:56 +0300

    initiated project

---
+++ SteamMarketplace/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+
+namespace SteamMarketplace
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+            services.AddMvc();
+           
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // app.UseHttpsRedirection();
+            //  app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            // app.MapRazorPages();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
