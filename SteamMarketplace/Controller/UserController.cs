962549e93a526344357d468f9153625eac7f854e
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-06-01 13:52:00 +0300

    created the user controller endpoint

---
+++ SteamMarketplace/Controller/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SteamMarketplace.Entities.DTO;
+using SteamMarketplace.Entities.Response;
+using SteamMarketplace.Services;
+
+namespace SteamMarketplace.Controller
+{
+    [ApiController]
+    [Route("api/v1/loginService")]
+    public class UserController : ControllerBase
+    {
+        private UserService _loginService;
+
+        public UserController(UserService loginService)
+        {
+            _loginService = loginService;
+        }
+        /* [HttpGet("{id}")]
+
+         public async Task<ActionResult> GetUsers(string id) 
+         {
+
+         }
+        */
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<Response> login([FromBody] UserDTO userDto)
+        {
+
+
+            return await _loginService.Usercheck(userDto);
+
+
+
+        }
+
+
+        [HttpPost]
+        [Route("signup")]
+        public async Task<Response> signup([FromBody] UserDTO userDto)
+        {
+
+
+            return await _loginService.CreateUser(userDto);
+
+
+
+        }
+    }
+}
