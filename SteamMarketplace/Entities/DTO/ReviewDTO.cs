5bde05cb452d6ff5dccb5917c13e6011aa5b228f
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-06-02 13:00:07 +0300

    created the review class and review DTO

---
+++ SteamMarketplace/Entities/DTO/ReviewDTO.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace SteamMarketplace.Entities.DTO
+{
+    public class ReviewDTO
+    {
+
+        [JsonProperty("rating")]
+        public string rating { get; set; }
+
+        [JsonProperty("rating")]
+        public string description { get; set; }
+    }
+}
