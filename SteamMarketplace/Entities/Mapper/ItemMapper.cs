31bc378a62d963d80d12a2e31887abdc01582236
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-06-06 17:24:42 +0300

    fixed the mapper

--- SteamMarketplace/Entities/Mapper/ItemMapper.cs
+++ SteamMarketplace/Entities/Mapper/ItemMapper.cs
@@ -1,17 +1,15 @@
 ﻿using AutoMapper;
+using SteamMarketplace.Entities;
 using SteamMarketplace.Entities.DTO;
 
 namespace SteamMarketplace.Entities.Mapper
 {
     public class ItemMapper : Profile
     {
-        public ItemMapper() {
-
+        public ItemMapper()
+        {
             CreateMap<Item, ItemDTO>();
             CreateMap<ItemDTO, Item>();
-
-
         }
-       
     }
 }
