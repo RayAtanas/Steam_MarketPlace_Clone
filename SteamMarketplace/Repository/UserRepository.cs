bbecb6ecb82065283ed71514377c3c56cb0730cf
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-07-13 15:19:21 +0300

    Updated the Find function

--- SteamMarketplace/Repository/UserRepository.cs
+++ SteamMarketplace/Repository/UserRepository.cs
@@ -1,7 +1,8 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using SteamMarketplace.Database;
 using SteamMarketplace.Entities;
-
+using Newtonsoft.Json;
 namespace SteamMarketplace.Repository
 {
     public class UserRepository
@@ -32,13 +33,28 @@ namespace SteamMarketplace.Repository
             return (await _context.users.FindAsync<User>(filter)).FirstOrDefault();
 
         }
+        public async Task<User> Findd(FilterDefinition<User> filter)
+        {
+           
+                return await _context.users.Find(filter).SingleOrDefaultAsync();
+            
+
+        }
+
+        public async Task<User> FindByEmail(string email)
+        {
+            FilterDefinition<User> filter = Builders<User>.Filter.Eq(u => u.Email, email);
+            User user = await _context.users.Find(filter).SingleOrDefaultAsync();
+            return user;
+        }
 
         public async Task<User> Get(string id)
         {
-            FilterDefinition<User> query = Builders<User>.Filter.Where(x => x.Id == id);
+            FilterDefinition<User> query = Builders<User>.Filter.Eq(x => x.Id, id);
 
-            return (await _context.users.FindAsync<User>(query)).FirstOrDefault();
+            return await _context.users.Find(query).SingleOrDefaultAsync();
         }
+
 
         public async Task Update(User user)
         {
