df3aa79310a257d3da1df130d7cd7cfa8bb1407d
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-07-13 15:17:56 +0300

    added the BSON elements

--- SteamMarketplace/Entities/User.cs
+++ SteamMarketplace/Entities/User.cs
@@ -1,32 +1,44 @@
-﻿using System.Text.Json.Serialization;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace SteamMarketplace.Entities
 {
     public class User : BaseEntity
     {
-        
-      
 
+
+        [BsonElement("firstname")]
         public string? FirstName { get; set; }
 
+        [BsonElement("lastName")]
         public string? LastName { get; set; }
 
+        [BsonElement("email")]
         public string Email { get; set; }
 
+        [BsonElement("password")]
         public string Password { get; set; }
 
+        [BsonElement("wallet")]
         public decimal? Wallet { get; set; }
-        
+
+        [BsonElement("inventory")]
         public Dictionary<string, Item>? Inventory { get; set; }
 
+        [BsonElement("transaction_History")]
         public TransactionHistory Transaction_History { get; set; }
 
+        [BsonElement("status")]
         public string? Status { get; set; }
 
+        [BsonElement("isBlocked")]
         public bool IsBlocked { get; set; }
 
+        [BsonElement("timeBlocked")]
         public DateTime TimeBlocked { get; set; }
 
+        [BsonElement("attempt")]
         public int Attempt { get; set; }
     }
 }
